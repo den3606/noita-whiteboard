6665ca5a458a8b789e38401d940365b450d6d246
den3606 <dedendendedenpunn@gmail.com>
2022-11-08 23:58:47 +0900

    refactor folder

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default {
   plugins: [crossOriginIsolation()],
   build: {
     outDir: '../dist',
-    assetsDir: './',
     rollupOptions: {
       output: {
         entryFileNames: 'bundle.js',
